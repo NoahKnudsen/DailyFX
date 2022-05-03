4e3a493917c9107b49737b7ce0f00b9a3aa6c2db
Noah Knudsen <KnudsenNoah@gmail.com>
2022-05-03 15:59:44 +0100

    Add more detail to NewsStory.Detail

--- DailyFX/Views/NewsStory+Detail.swift
+++ DailyFX/Views/NewsStory+Detail.swift
@@ -4,6 +4,7 @@
 
 import DailyFXService
 import SwiftUI
+import Toolkit
 
 extension NewsStory {
     
@@ -12,7 +13,60 @@ extension NewsStory {
         let story: NewsStory
         
         var body: some View {
-            Text(story.title)
+            ScrollView {
+                
+                AsyncImage(
+                    url: story.headlineImageUrl,
+                    content: { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    },
+                    placeholder: {
+                        Rectangle()
+                            .fill(Color.gray)
+                            .overlay{
+                                Image(systemName: "photo")
+                                    .foregroundColor(Color(UIColor.lightGray))
+                                    .font(.title)
+                            }
+                            .frame(minHeight: 150)
+                    }
+                )
+                .frame(maxWidth: .infinity)
+                
+                VStack(spacing: 15) {
+                    
+                    Text(story.title)
+                        .font(.largeTitle.bold())
+                    
+                    LabelGroup.list {
+                        LabelGroup.hiddenIfNil(
+                            title: "Authors",
+                            value: story.authors
+                                .map(\.name)
+                                .joined(separator: ", ")
+                                .ifNotEmpty
+                        )
+                        
+                        LabelGroup.hiddenIfNil(
+                            title: "Description",
+                            value: story.description
+                        )
+                        
+                        LabelGroup.hiddenIfNil(
+                            title: "Tags",
+                            value: story.tags
+                                .sorted(by: {$0})
+                                .joined(separator: ", ")
+                                .ifNotEmpty
+                        )
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(story.title)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
---
+++ Packages/Foundational/Sources/Foundational/Collection.swift
@@ -0,0 +1,33 @@
+//
+//  Created by Noah Knudsen on 01/05/2022.
+//
+
+import Foundation
+
+public extension Collection {
+    
+    var ifNotEmpty: Self? { isEmpty ? nil : self }
+}
+
+public extension Collection {
+    
+    func sorted<T: Comparable>(
+        by f: (Element) throws -> T,
+        order: SortDirection = .accending
+    ) rethrows -> [Element] {
+        let compare = order.comparing(T.self)
+        return try sorted(by: { try compare(f($0), f($1)) })
+    }
+}
+
+public enum SortDirection {
+    
+    case accending, decending
+    
+    func comparing<T: Comparable>(_ type: T.Type) -> (T, T) -> Bool {
+        switch self {
+        case .accending: return (<)
+        case .decending: return (>)
+        }
+    }
+}
