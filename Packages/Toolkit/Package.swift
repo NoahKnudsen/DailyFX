7973fa5cd4a80719de5c5bcd71cdb1a19567c405
Noah Knudsen <KnudsenNoah@gmail.com>
2022-05-03 14:37:52 +0100

    Add Toolkit package

---
+++ Packages/Toolkit/Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "Toolkit",
+    platforms: [.macOS("12.0"), .iOS("13.0")],
+    products: [
+        .library(name: "Toolkit", targets: ["Toolkit"]),
+    ],
+    dependencies: [.package(path: "../Foundational")],
+    targets: [
+        .target(
+            name: "Toolkit",
+            dependencies: ["Foundational"]
+        ),
+        .testTarget(
+            name: "ToolkitTests",
+            dependencies: [
+                "Toolkit",
+                "Foundational"
+            ],
+            resources: []
+        ),
+    ]
+)
---
+++ Packages/Toolkit/Sources/Toolkit/Observable.swift
@@ -0,0 +1,91 @@
+//
+//  Created by Noah Knudsen on 01/05/2022.
+//
+
+import SwiftUI
+
+@dynamicMemberLookup
+public class Observable<Value>: ObservableObject {
+    
+    @Published public var state: State<Value> = .noValue
+ 
+    public init(_ stream: AsyncThrowingStream<Value, Error>) {
+        Task {
+            await subscribe(to: stream)
+        }
+    }
+    
+    @MainActor private func subscribe(to stream: AsyncThrowingStream<Value, Error>) async {
+        do {
+            for try await value in stream {
+                self.state = .value(value)
+            }
+        } catch {
+            self.state = .error(error)
+        }
+    }
+    
+    public subscript<T>(dynamicMember keyPath: KeyPath<Value, T>) -> T? {
+        value?[keyPath: keyPath]
+    }
+    
+    public subscript<T>(dynamicMember keyPath: KeyPath<Value, T?>) -> T?
+    {
+        value?[keyPath: keyPath].flatMap{ $0 }
+    }
+}
+
+public extension Observable {
+    
+    enum State<Value> {
+        case noValue
+        case value(Value)
+        case error(Error)
+    }
+    
+    var value: Value? { state.value }
+}
+
+public extension Observable.State {
+    
+    var value: Value? {
+        switch self {
+        case .value(let o): return o
+        default: return nil
+        }
+    }
+}
+
+public extension Observable {
+    
+    /// Provide content, placeholder, and error views for the Obeservable's state
+    @ViewBuilder
+    func view<Placeholder, Error, Content>(
+        @ViewBuilder placeholder: () -> Placeholder,
+        @ViewBuilder error: (Swift.Error) -> Error,
+        @ViewBuilder content: (Value) -> Content
+    ) -> some View
+    where Placeholder: View, Error: View, Content: View
+    {
+        switch state {
+        case .noValue: placeholder()
+        case .error(let e): error(e)
+        case .value(let v): content(v)
+        }
+    }
+    
+    /// Provide a content view for the value state of the observable, and
+    /// Empty views for when theres no value or an error
+    @ViewBuilder
+    func view<Content>(
+        @ViewBuilder _ content: (Value) -> Content
+    ) -> some View
+    where Content: View
+    {
+        view(
+            placeholder: { EmptyView() },
+            error: { _ in EmptyView() },
+            content: content
+        )
+    }
+}
